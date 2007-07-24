89ddad01ea3b5d8a6b6c734c8ffec5b9098001ca
caetanominuzzo <caetanominuzzo@5b290fc2-902f-0410-adb6-a1021d1c696a>
2007-07-24 13:54:42 +0000

    made a copy

---
+++ branches/pProject/pNeuronIDE/pNeuronIDE.cs
@@ -0,0 +1,258 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
+using primeira.pNeuron.Core;
+using System.IO;
+
+namespace primeira.pNeuron
+{
+    public partial class pNeuronIDE : Form
+    {
+        public pProperty fmProperty = new pProperty();
+        public pToolbox fmToolbox = new pToolbox();
+        public pGroupExplorer fmGroupExplorer = new pGroupExplorer();
+        //DEPRECATEDpublic pNetworkExplorer fmNetworkExplorer = new pNetworkExplorer();
+        public pPlotter fmPlotter = new pPlotter();
+
+
+        public List<pDocument> fmDocuments = new List<pDocument>();
+        private pDocument fActiveDocument;
+
+        public pDocument GetDocByName(String sName)
+        {
+            foreach (pDocument p in fmDocuments)
+                if (p.Filename == sName)
+                    return p;
+
+            return null;
+        }
+
+        public pDocument ActiveDocument
+        {
+            get
+            {
+                if (fActiveDocument == null)
+                {
+                    pMessage.Alert("Please create a new document or open one before try this.");
+                    return null;
+                }
+
+                return fActiveDocument;
+            }
+            internal set
+            {
+                fmToolbox.SetToolSet(value);
+                fActiveDocument = value;
+            }
+        }
+
+        public bool ActiveDocumentExists()
+        {
+            return fActiveDocument != null;
+        }
+
+
+        public void SetActiveDocument(pDocument aActiveDocument)
+        {
+            ActiveDocument = aActiveDocument;
+        }
+
+        /// <summary>
+        /// This don't really closes the pDoc object since this method is called by it.
+        /// Just change de ActiveDocument to an open or set it to null and closes sub items.
+        /// </summary>
+        /// <param name="aRemoveDocument"></param>
+        public void PreRemoveDocument(pDocument aRemoveDocument)
+        {
+            fmDocuments.Remove(aRemoveDocument);
+            fmGroupExplorer.treeView1.Clear();
+
+            //DEPRECATED
+            //if(fmNetworkExplorer.treeView1.Nodes!=null)
+            //    if(fmNetworkExplorer.treeView1.Nodes[aRemoveDocument.Filename]!=null)
+            //        foreach (TreeNode n in fmNetworkExplorer.treeView1.Nodes[aRemoveDocument.Filename].Nodes)
+            //        {
+            //            pDocument p = GetDocByName(n.Name);
+            //            p.QueryOnClose = false;
+            //            p.Close();
+            //        }
+
+            //fmNetworkExplorer.RemoveNode(aRemoveDocument.Filename);
+
+            if (fmDocuments.Count > 0)
+            {
+                ActiveDocument = fmDocuments[fmDocuments.Count - 1];
+            }
+            else
+                ActiveDocument = null;
+
+        }
+
+
+        public pNeuronIDE()
+        {
+            InitializeComponent();
+
+            fmToolbox.Show(dockPanel, DockState.DockLeft);
+
+            fmPlotter.Show(dockPanel, DockState.DockBottomAutoHide);
+
+            fmGroupExplorer.Show(dockPanel, DockState.DockRight);
+
+            //DEPRECATEDfmNetworkExplorer.Show(dockPanel, DockState.DockRight);
+            //DEPRECATEDfmNetworkExplorer.DockTo(fmGroupExplorer.Pane, DockStyle.Fill, 0);
+
+            fmProperty.Show(dockPanel, DockState.DockRight);
+            fmProperty.DockTo(fmGroupExplorer.Pane, DockStyle.Bottom, 0);
+
+            //Just to keep consistency
+            ActiveDocument = null;
+
+        }
+
+        public void OpenAny(string sFilename, TreeNode FilenameParent)
+        {
+            foreach (pDocument p in fmDocuments)
+            {
+                if (p.Filename == sFilename)
+                {
+                    p.Show();
+                    return;
+                }
+            }
+
+            switch (Path.GetExtension(sFilename))
+            {
+                case ".pne": fmDocuments.Add(new pDocument(sFilename));
+                    ActiveDocument = fmDocuments[fmDocuments.Count - 1];
+                    ((pDocument)ActiveDocument).Show(dockPanel, DockState.Document);
+                    ((pDocument)ActiveDocument).internalLoad(((pDocument)ActiveDocument).Filename);
+                    break;
+                    
+
+            }
+            
+            ActiveDocument.Modificated = false;
+            ActiveDocument.DefaultNamedFile = false;
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            //Avoid base
+        }
+
+        #region ToolStrip
+
+        //FILE
+
+        private void newNetworkToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            NewNetwork();
+        }
+
+        private void openNetworkToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenNetwork();
+        }
+
+        private void saveToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            SaveNetwork();
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveNetworkAs();
+        }
+
+        //VIEW
+        private void propertyWindowToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fmProperty.Show();
+        }
+
+        private void networkExplorerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fmGroupExplorer.Show();
+        }
+
+        private void toolBoxToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fmToolbox.Show();
+        }
+
+        //TRAIN
+        private void trainToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pTrain fmTrain = new pTrain();
+            fmTrain.net = ((pDocument)ActiveDocument).pDisplay1.Net;
+            fmTrain.Show();
+        }
+
+        #endregion
+
+        private void pNeuronIDE_Load(object sender, EventArgs e)
+        {
+            //Create Environment
+            if (!Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.Personal) + "\\pNeuron Projects"))
+            {
+                Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.Personal) + "\\pNeuron Projects");
+            }
+        }
+
+        #region New/Open/Save network
+
+        public void NewNetwork()
+        {
+            int i = fmDocuments.Count + 1;
+            fmDocuments.Add(new pDocument("NeuralNetwork " + i.ToString()));
+            ActiveDocument = fmDocuments[fmDocuments.Count - 1];
+            ((pDocument)ActiveDocument).Show(dockPanel, DockState.Document);
+            ((pDocument)ActiveDocument).Modificated = true;
+            //DEPRECATEDfmNetworkExplorer.AddNode(((pDocument)ActiveDocument).Filename);
+        }
+
+        public void OpenNetwork()
+        {
+            pDocument p = new pDocument();
+            p.Parent = this;
+            if (p.Load() == DialogResult.OK)
+            {
+                fmDocuments.Add(p);
+                ActiveDocument = fmDocuments[fmDocuments.Count - 1];
+                ((pDocument)ActiveDocument).Show(dockPanel, DockState.Document);
+                ((pDocument)ActiveDocument).Modificated = false;
+            }
+            //DEPRECATEDfmNetworkExplorer.AddNode(((pDocument)ActiveDocument).Filename);
+        }
+        
+        public void SaveNetwork()
+        {
+            ActiveDocument.Save();
+        }
+
+        public void SaveNetworkAs()
+        {
+            bool bDefaul = ActiveDocument.DefaultNamedFile;
+            string old = ActiveDocument.Filename;
+
+            ActiveDocument.DefaultNamedFile = true;
+            if (ActiveDocument.Save() == DialogResult.Cancel)
+            {
+                ActiveDocument.DefaultNamedFile = bDefaul;
+            }
+
+        }
+
+        
+
+        #endregion
+        
+
+    }
+}
---
+++ branches/pProject/pNeuronIDE/pTrain.cs
@@ -0,0 +1,262 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using primeira.pNeuron.Core;
+
+namespace primeira.pNeuron
+{
+    public partial class pTrain : Form
+    {
+        public pTrain()
+        {
+            InitializeComponent();
+        }
+
+        public NeuralNet net = new NeuralNet();
+
+        TextBox[][] t = new TextBox[3][];
+           double high, mid, low;
+
+        int gI = 0;
+
+
+
+        private void pTrain_Load(object sender, EventArgs e)
+        {
+            high = .9999999999999999999;
+
+            low = .00000000000000000001;
+
+            mid = .5;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double[][] input = new double[4][];
+            input[0] = new double[] {high, high};
+            input[1] = new double[] {low, high};
+            input[2] = new double[] {high, low};
+            input[3] = new double[] {low, low};
+
+            double[][] output = new double[4][];
+            output[0] = new double[] { low };
+            output[1] = new double[] { high };
+            output[2] = new double[] { high };
+            output[3] = new double[] { low };
+
+          //  net.Initialize(1, 0);
+        
+            double ll, lh, hl, hh;
+
+            int count;
+
+            count = 0;
+            net.LearningRate = .5;
+            
+            net.InitializeLearning();
+
+            Timer t = new Timer();
+            t.Interval = 1;
+            t.Tick += new EventHandler(t_Tick);
+            t.Enabled = true;
+            double dGlobalError = 0;
+            double dTotalError = 0;
+            do
+            {
+
+                count++;
+
+
+                net.Train(input, output, 1);
+
+                dTotalError = 0;
+                foreach (Neuron n in net.Neuron)
+                {
+                    dTotalError += n.Error;
+                }
+
+                dGlobalError = dTotalError / net.Neuron.Count;
+
+
+                // net.ApplyLearning();
+
+                net.Neuron[0].Value = low;
+                net.Neuron[1].Value = low;
+
+                net.Pulse();
+                ll = net.Neuron[net.Neuron.Count - 1].Value;
+
+
+                net.Neuron[0].Value = high;
+                net.Neuron[1].Value = low;
+
+                net.Pulse();
+                hl = net.Neuron[net.Neuron.Count - 1].Value;
+
+
+                net.Neuron[0].Value = low;
+                net.Neuron[1].Value = high;
+
+                net.Pulse();
+                lh = net.Neuron[net.Neuron.Count - 1].Value;
+
+
+                net.Neuron[0].Value = high;
+                net.Neuron[1].Value = high;
+
+                net.Pulse();
+                hh = net.Neuron[net.Neuron.Count - 1].Value;
+
+            }
+
+            //while (hh > (mid + low) / 2
+            //    || lh < (mid + high) / 2
+            //    || hl < (mid + low) / 2
+            //    || ll > (mid + high) / 2);
+
+
+            while ( dGlobalError  < -.0001 || dGlobalError > .0001);
+
+            StringBuilder bld = new StringBuilder();
+
+            bld.Remove(0, bld.Length);
+            bld.Append((count * 5).ToString()).Append(" iterations required for training\n");
+
+            bld.Append("hh: ").Append(hh.ToString()).Append(" < .5\n");
+            bld.Append("ll: ").Append(ll.ToString()).Append(" < .5\n");
+
+            bld.Append("hl: ").Append(hl.ToString()).Append(" > .5\n");
+            bld.Append("lh: ").Append(lh.ToString()).Append(" > .5\n");
+            bld.Append("GLOBAL " + dGlobalError.ToString());
+            bld.Append(gI.ToString());
+            MessageBox.Show(bld.ToString());
+
+         //   net.Train(input, output, TrainingType.BackPropogation, 100);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            net.Neuron[0].Value = Convert.ToDouble(textBox1.Text);
+            if (net.Neuron[1].NeuronType == NeuronTypes.Input)
+                net.Neuron[1].Value = Convert.ToDouble(textBox2.Text);
+
+            net.Pulse();
+
+                MessageBox.Show(net.Neuron[4].Value.ToString());
+        }
+
+        void t_Tick(object sender, EventArgs e)
+        {
+            gI++;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double[][] input = new double[10][];
+            input[0] = new double[] { 1 };
+            input[1] = new double[] { 2 };
+            input[2] = new double[] { 3 };
+            input[3] = new double[] { 4 };
+            input[4] = new double[] { 5 };
+            input[5] = new double[] { 6 };
+            input[6] = new double[] { 7 };
+            input[7] = new double[] { 8 };
+            input[8] = new double[] { 9 };
+            input[9] = new double[] { 0 };
+
+            double[][] output = new double[10][];
+            output[0] = new double[] { low, high };
+            output[1] = new double[] { high, low };
+            output[2] = new double[] { low, high };
+            output[3] = new double[] { high, low };
+            output[4] = new double[] { low, high };
+            output[5] = new double[] { high, low };
+            output[6] = new double[] { low, high };
+            output[7] = new double[] { high, low };
+            output[8] = new double[] { low, high };
+            output[9] = new double[] { high, low };
+
+            //  net.Initialize(1, 0);
+
+            double ll, lh, hl, hh;
+
+            int count;
+
+            count = 0;
+            net.LearningRate = 1;
+
+            net.InitializeLearning();
+
+            Timer t = new Timer();
+            t.Interval = 1;
+            t.Tick += new EventHandler(t_Tick);
+            t.Enabled = true;
+
+            do
+            {
+
+                count++;
+
+
+
+
+
+                net.Train(input, output, 100);
+
+                // net.ApplyLearning();
+
+                bool bOk = true;
+
+                for (int i = 1; i < 10; i++ )
+                {
+                    net.Neuron[0].Value = i;
+                    net.Pulse();
+                    if ((i % 2 == 0 && net.Neuron[net.Neuron.Count - 2].Value < net.Neuron[net.Neuron.Count - 1].Value)
+                        ||
+                        (i % 2 == 1 && net.Neuron[net.Neuron.Count - 2].Value > net.Neuron[net.Neuron.Count - 1].Value))
+                    {
+                        bOk = false;
+                        break;
+                    }
+
+                }
+
+                if (bOk)
+                    break;
+            }
+
+            while (true);
+
+            StringBuilder bld = new StringBuilder();
+
+            bld.Remove(0, bld.Length);
+            bld.Append((count * 5).ToString()).Append(" iterations required for training\n");
+
+            //bld.Append("hh: ").Append(hh.ToString()).Append(" < .5\n");
+            //bld.Append("ll: ").Append(ll.ToString()).Append(" < .5\n");
+
+            //bld.Append("hl: ").Append(hl.ToString()).Append(" > .5\n");
+            //bld.Append("lh: ").Append(lh.ToString()).Append(" > .5\n");
+            bld.Append(gI.ToString());
+            MessageBox.Show(bld.ToString());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            net.Neuron[0].Value = Convert.ToDouble(textBox3.Text);
+
+            net.Pulse();
+
+            if(net.Neuron[net.Neuron.Count-2].Value > net.Neuron[net.Neuron.Count-1].Value)
+                MessageBox.Show("Impar");
+            else MessageBox.Show("Par");
+
+        }
+
+    }
+}
